e6719efc6034b9828f70272e14abe590c58bbbaf
christianvedels <christian-vs@sam.sdu.dk>
2023-04-12 17:58:13 +0200

    Key done

--- 002_Linking_geo_data.R
+++ 002_Linking_geo_data.R
@@ -7,9 +7,72 @@
 # ==== Libraries and preparation ====
 library(tidyverse)
 source("000_functions.R")
-library(sf)
+library(rgdal)
+
+# ==== Read data ====
+load("Data/tmp_census.Rdata")
 
 # ==== Shape files ====
-shape_old = readOGR("../../Conflict, Religion, and Development/WP1 - Religious conflict in Denmark/Maps fra Nina/Sogne_shape/Sogn1820Final_Adjust.shp")
-shape_digdag = readOGR("../../Conflict, Religion, and Development/WP1 - Religious conflict in Denmark/Public map data/KIRKELIG_SHAPE_UTM32-EUREF89/Sogn.shp")
-shape_digdag = shape_digdag %>% subset(fra < "1820-01-01" & til > "1820-01-01")
+shape_parish = readOGR("Data/sogne_shape") # From www.digdag.dk
+
+# ==== Extract data to make manual key ====
+# merged_data %>% # Tmp 1 for making key
+#   distinct(event_county, event_district, event_parish) %>% 
+#   mutate_all(.funs = sub_scandi) %>% 
+#   write_csv2("Tmp1.csv")
+# 
+# shape_parish@data %>% # Tmp 2 for making key
+#   select(AMT, HERRED, SOGN, GIS_ID, lat, long) %>% 
+#   mutate_all(.funs = sub_scandi) %>% 
+#   mutate(
+#     string = paste(AMT, SOGN, GIS_ID, sep = ", ")
+#   ) %>% 
+#   write_csv2("Tmp2.csv")
+
+# Read the finished key
+key = read_csv2("Data/Key_census_to_shape.csv")
+
+# ==== GIS ID to census data ====
+# 'GIS_ID' is the name of the unique ids in the shape file
+key %>% 
+  rowwise() %>% 
+  mutate(
+    GIS_ID = strsplit(GIS_ID, ", ")[[1]][3]
+  )
+
+stop("Lemvig is missing in LL data")
+
+
+key = key %>%
+  select(Amt, Herred, Sogn, GIS_ID) %>% 
+  rowwise() %>% 
+  mutate(GIS_ID = gsub("(.*),.*", "\\1", GIS_ID))
+
+match_space %>% 
+  mutate(GIS_ID = as.character(GIS_ID)) %>% 
+  anti_join(key, by = "GIS_ID") #%>% View()
+
+
+# key stats
+n_for_missing = merged_data %>%
+  group_by(Year, County, Hundred, Placename) %>%
+  count() %>%
+  rename(
+    Sogn = Placename,
+    Amt = County,
+    Herred = Hundred
+  ) %>%
+  arrange(Amt, Herred) %>%
+  semi_join(
+    key %>% filter(is.na(GIS_ID))
+  )
+
+# add key to merged_data 
+n1 = NROW(merged_data)
+tmp = merged_data %>% 
+  left_join(key, c("County"="Amt","Placename"="Sogn","Hundred"="Herred"))
+
+n2 = NROW(tmp)
+if(n1!=n2) stop("Something went wrong in key joining")
+merged_data = tmp
+rm(tmp)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# A_perfect_storm_replication
- Replication files for a perfect storm
+# Replication files for "A perfect storm and the natural endowments of trade-enabling infrastructure"
+
+## Data in this repository
+- sogne_shape: Shapefile from www.digdag.dk
+- Key_census_to_shape.csv: This is a key, which links census data locations to the shape file
+
+## Data I cannot redistribute
+The entire project is based on publicly available data. However, some if it I am not allowed to redistribute directlty. Links for these data sources are included here:
+- **Danish census data 1787-1901:**:
+https://www.rigsarkivet.dk/udforsk/link-lives-data/ 
+- **Sound toll registers**: They will send you the full data on request. See more here: http://www.soundtoll.nl/index.php/en/over-het-project/str-online 
+
