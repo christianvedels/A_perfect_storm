1313c21cda0ec8bc316f4aeba26ee36598ed55fd
christianvedels <christian-vs@sam.sdu.dk>
2023-04-14 15:39:12 +0200

    Create 004_Aggregate_demographic_data.R

---
+++ 004_Aggregate_demographic_data.R
@@ -0,0 +1,13 @@
+# Aggregate demographic data
+#
+# Date updated:   2023-04-14
+# Auhtor:         Christian Vedel 
+# Purpose:        Join on data on HISCO codes and summarise
+#
+# Output:         'popdata.csv', demographic data aggregated to parish level 
+
+# ==== Libraries ====
+library(tidyverse)
+source("000_Functions.R")
+
+# ==== Load data ====
