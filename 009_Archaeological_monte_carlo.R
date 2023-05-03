687666489d0ed3309082f46e104482977b50f998
christianvedels <christian-vs@sam.sdu.dk>
2023-05-03 13:30:16 +0200

    Update 009_Archaeological_monte_carlo.R
    Added the normal distribution as an alternative to the uniform distribution

--- 009_Archaeological_monte_carlo.R
+++ 009_Archaeological_monte_carlo.R
@@ -91,6 +91,71 @@ monteCarlo = function(Finding_types, capB = 1000, resoultion = 50){
   )
 }
 
+# Same as above but based on a normal distribution rather than uniform
+monteCarlo_norm = function(Finding_types, capB = 1000, resoultion = 50){
+  the_data0 = the_data %>% 
+    filter(finding_interpretation_en %in% Finding_types)
+  if(NROW(the_data0)==0){stop("No findings for 'Finding_types'")}
+  
+  count_boot = foreach(b = 1:capB, .combine = "bind_rows") %do% {
+    # Sample time interval from that coin
+    suppressMessages({
+      res = the_data0 %>%
+        mutate(
+          mean0 = (From_year+To_year)/2,
+          sd0 = (To_year - From_year)/1.96
+        ) %>% 
+        mutate(
+          rYear = round(rnorm(n(), mean0, sd0)/resoultion)*resoultion
+        ) %>%
+        group_by(GIS_ID, rYear) %>%
+        summarise(
+          n = n()
+        ) %>%
+        mutate(
+          activity = as.numeric(n>1)
+        )
+    })
+    
+    # Joining with GIS IDs which never showed up
+    res = expand.grid(
+      GIS_ID = as.character(geo_data$GIS_ID),
+      rYear = seq(750, 1500, by = resoultion)
+    ) %>%
+      left_join(
+        res %>% mutate(GIS_ID = as.character(GIS_ID)), by = c("GIS_ID", "rYear")
+      ) %>%
+      mutate(
+        activity = ifelse(is.na(activity), 0, activity),
+        n = ifelse(is.na(n), 0, n),
+      ) %>%
+      mutate(b = b)
+    
+    return(res)
+  }
+  
+  suppressMessages({
+    res = count_boot %>% 
+      group_by(rYear, GIS_ID) %>% 
+      summarise(
+        n = sum(n),
+        success = sum(activity),
+        trials = capB
+      ) %>% 
+      mutate(
+        rate = success/trials,
+        rate_n = n/trials
+      )
+  })
+  
+  return(
+    list(
+      panel = res,
+      samples = count_boot
+    )
+  )
+}
+
 
 # ==== Run MC ====
 # params
@@ -137,6 +202,41 @@ arch_data = foreach(i = unique(site_types_tab$Category)) %do% {
   }
   save(res_is, file = paste0("Data/Tmp_arch_samples/", i,".Rdata") )
 
+  return(res_is)
+}
+
+# loop
+set.seed(20)
+arch_data_norm = foreach(i = unique(site_types_tab$Category)) %do% {
+  cat("\n", i, "\n")
+  finding_types_i = site_types_tab %>% 
+    filter(Category == i) %>% 
+    select(finding_interpretation_en) %>% 
+    unlist() %>% unname()
+  
+  res_i = monteCarlo(Finding_types = finding_types_i, capB = capB)
+  
+  res_is = foreach(j = finding_types_i) %do% {
+    suppressWarnings({
+      j0 = assertive.strings:::strip_non_alphanumeric(j)
+    })
+    cat(
+      "--->", as.character(Sys.time()), j, 
+      "                                                      \r"
+    )
+    res_j = monteCarlo(Finding_types = j, capB = capB)
+    return(res_j)
+  }
+  names(res_is) = finding_types_i
+  res_is[[length(res_is)+1]] = res_i
+  names(res_is)[length(res_is)] = paste0("Overall_", i)
+  
+  dir_i = paste0("Data/Tmp_arch_samples_norm")
+  if(!dir.exists(dir_i)){
+    dir.create(dir_i)
+  }
+  save(res_is, file = paste0("Data/Tmp_arch_samples_norm/", i,".Rdata") )
+  
   return(res_is)
 }
 
