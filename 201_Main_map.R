0e458769117b0d2052fce63e6e16e26ffd259cd6
christianvedels <christian.vedel.soerensen@gmail.com>
2025-05-19 14:00:16 +0200

    Update map text

--- 201_Main_map.R
+++ 201_Main_map.R
@@ -1,5 +1,5 @@
 # Main map
-# Date updated:   2024-06-16
+# Date updated:   2025-05-19
 # Auhtor:         Christian Vedel 
 # Purpose:        Construct the map showing the event    
 #
@@ -10,7 +10,7 @@ library(tidyverse)
 library(ggspatial)
 
 # ==== Load data ====
-shape = rgdal::readOGR("Data/sogne_shape")
+shape = sf::st_read("Data/sogne_shape")
 geo_data = read_csv2("Data/Geo.csv", guess_max = 2000)
 parishMA = read_csv2("Data/MA_estimates.csv", guess_max = 2000)
 market_towns = read.csv2("Data/Market_towns.csv", fileEncoding="latin1")
@@ -34,7 +34,7 @@ parishMA = parishMA %>%
   filter(theta == -1, alpha == 10) %>% 
   mutate(dif_lMA = log(MA_after_before))
 
-shape@data = shape@data %>% 
+shape = shape %>% 
   left_join(
     geo_data %>% 
       select(GIS_ID, limfjord_placement) %>% 
@@ -72,74 +72,74 @@ p1 = ggplot() +
     aes(x = 8.2, y = 56.71, shape = "Agger channel"), 
     size = 5
   ) +
-  geom_curve( # Period 1200 to 1834
+  geom_curve( # Period 1200 to 1825
     data = data.frame(
       y = 56.550615, x = 8.305105,
       yend = 56.970915, xend = 9.207246
     ),
     aes(
       x = x, y = y, xend = xend, yend = yend,
-      lty = "Period 1200 to 1834"
+      lty = "Period 1200 to 1825"
     ),
     curvature = 0.1,
     arrow = arrow(angle = 20)
   ) +
-  geom_curve( # Period 1200 to 1834
+  geom_curve( # Period 1200 to 1825
     data = data.frame(
       y = 56.970915, x = 9.207246,
       yend = 56.947637, xend = 10.488074
     ),
     aes(
       x = x, y = y, xend = xend, yend = yend,
-      lty = "Period 1200 to 1834"
+      lty = "Period 1200 to 1825"
     ),
     curvature = -0.2,
     arrow = arrow(angle = 20)
   ) +
-  geom_curve( # Period 1200 to 1834
+  geom_curve( # Period 1200 to 1825
     data = data.frame(
       y = 56.947637, x = 10.488074,
       yend = 57.800385, xend = 10.808764
     ),
     aes(
       x = x, y = y, xend = xend, yend = yend,
-      lty = "Period 1200 to 1834"
+      lty = "Period 1200 to 1825"
     ),
     curvature = 0.3,
     arrow = arrow(angle = 20)
   ) +
-  geom_curve( # Period 1200 to 1834
+  geom_curve( # Period 1200 to 1825
     data = data.frame(
       y = 57.800385, x = 10.808764,
       yend = 57.641009, xend = 8.883548
     ),
     aes(
       x = x, y = y, xend = xend, yend = yend,
-      lty = "Period 1200 to 1834"
+      lty = "Period 1200 to 1825"
     ),
     curvature = 0.3,
     arrow = arrow(angle = 20)
   ) +
-  geom_curve( # Period 1200 to 1834
+  geom_curve( # Period 1200 to 1825
     data = data.frame(
       y = 57.641009, x = 8.883548,
       yend = 57.2, xend = 7.4
     ),
     aes(
       x = x, y = y, xend = xend, yend = yend,
-      lty = "Period 1200 to 1834"
+      lty = "Period 1200 to 1825"
     ),
     curvature = -0.2,
     arrow = arrow(angle = 20)
   ) +
-  geom_curve( # After 1834
+  geom_curve( # After 1825
     data = data.frame(
       y = 56.550615, x = 8.305105,
       yend = 56.9, xend = 7.4
     ),
     aes(
       x = x, y = y, xend = xend, yend = yend,
-      lty = "After 1834"
+      lty = "After 1825"
     ),
     curvature = 0.4,
     arrow = arrow(angle = 20)
@@ -164,7 +164,7 @@ p1 = ggplot() +
 
 p2 = p1 +
   theme(
-    plot.background = element_rect(fill = "white", size = 0)
+    plot.background = element_rect(fill = "white", linewidth = 0)
   )
 
 
