50f40561f3b169f800069e552b562ad308db5de4
christianvedels <christian.vedel.soerensen@gmail.com>
2026-03-24 19:18:25 +0100

    Replaced remnants of rgdal dependence

--- 005_Limfjord_regions .R
+++ 005_Limfjord_regions .R
@@ -6,18 +6,21 @@
 # Output:         Geo.csv containing GIS ID and their Limfjord regions
 
 
+# ==== Options ====
+if(!exists("OVERWRITE")) OVERWRITE = 1  # 0: never overwrite; 1: overwrite if older than 7 days; 2: always overwrite
+
 # ==== Libraries ====
 library(tidyverse)
 source("000_Functions.R")
 library(sf)
+library(sp)
 library(ggspatial)
 
 # ==== Load data ====
 # - https://osmdata.openstreetmap.de/data/water-polygons.html
+sf_use_s2(FALSE) # Shapefiles have invalid rings rejected by S2; keep off until after clip
 water_line = as(st_read("Data/water-polygons-split-4326"), "Spatial")
-
 shape_parish = as(st_read("Data/sogne_shape"), "Spatial") # From www.digdag.dk
-
 limfjord = as(st_read("Data/limfjorden/Limfjorden.shp"), "Spatial")
 
 # ==== Cleaning shape files ====
@@ -26,11 +29,10 @@ limfjord = as(st_read("Data/limfjorden/Limfjorden.shp"), "Spatial")
 b = matrix(c(8, 54, 16, 58),nrow=2)
 
 water_dk = Clip_it(water_line, b)
-plot(water_dk)
 
 # Transform to same proj4string
-water_dk = water_dk %>% spTransform("+proj=longlat +zone=32 +ellps=GRS80")
-limfjord = limfjord %>% spTransform("+proj=longlat +zone=32 +ellps=GRS80")
+water_dk = as(sf::st_transform(sf::st_as_sf(water_dk), "+proj=longlat +zone=32 +ellps=GRS80"), "Spatial")
+limfjord  = as(sf::st_transform(sf::st_as_sf(limfjord),  "+proj=longlat +zone=32 +ellps=GRS80"), "Spatial")
 
 # Construct spdf from parish shape
 sogne_spdf = SpatialPointsDataFrame(
@@ -41,14 +43,19 @@ sogne_spdf = SpatialPointsDataFrame(
 
 # ==== Calculate distances ====
 # The following takes ~18 hours to run. This motivates the tmp file
-# # 1. Calculate dist to ocean
-# # 2. Calculate dist to limfjord
-# 
-# parish_to_limfjord = geosphere::dist2Line(sogne_spdf, limfjord)
-# parish_to_ocean = geosphere::dist2Line(sogne_spdf, water_dk)
-# 
-# save(parish_to_limfjord, parish_to_ocean, file = "Data/tmp_limfj_distances0.Rdata")
-load("Data/tmp_limfj_distances0.Rdata")
+fpath_dist = "Data/tmp_limfj_distances0.Rdata"
+# Distance computation takes ~18 hours; only redo if file missing (OVERWRITE=1) or forced (OVERWRITE=2)
+dist_exists = file.exists(fpath_dist)
+
+if(OVERWRITE == 2 || (OVERWRITE == 1 && !dist_exists)){
+  cat("Computing distances (this takes ~18 hours)...\n")
+  parish_to_limfjord = geosphere::dist2Line(sogne_spdf, limfjord)
+  parish_to_ocean = geosphere::dist2Line(sogne_spdf, water_dk)
+  save(parish_to_limfjord, parish_to_ocean, file = fpath_dist)
+} else {
+  cat("Loading cached distances\n")
+  load(fpath_dist)
+}
 
 parish_to_limfjord = data.frame(parish_to_limfjord) %>% 
   # mutate(STEDNR = sogne_spdf$STEDNR) %>% 
@@ -75,29 +82,31 @@ shape_parish@data = shape_parish@data %>%
 
 
 # ==== Plots sannity check ====
-p1 = ggplot() + 
+if(!dir.exists("tmp_plots")) dir.create("tmp_plots")
+
+p1 = ggplot() +
   layer_spatial(
     data = shape_parish,
     aes(fill = distance_oce)
   ) +
   ggtitle("Distance to the ocean")
-p1
+ggsave("tmp_plots/05_tmp1.png", p1)
 
-p2 = ggplot() + 
+p2 = ggplot() +
   layer_spatial(
     data = shape_parish,
     aes(fill = log(distance_lim))
   ) +
   ggtitle("Distance to the Limfjord")
-p2
+ggsave("tmp_plots/05_tmp2.png", p2)
 
-p3 = ggplot() + 
+p3 = ggplot() +
   layer_spatial(
     data = shape_parish,
     aes(fill = limfjord_closest)
   ) +
   ggtitle("Limfjord closest")
-p3
+ggsave("tmp_plots/05_tmp3.png", p3)
 
 # Missing. Looked up on https://digdag.dk/
 # - Harbooer
@@ -133,22 +142,22 @@ shape_parish@data = shape_parish@data %>%
     dist_east_west = dist2line0(c(lat, long))
   )
 
-# Check on plot 
-p1 = ggplot() + 
+# Check on plot
+p1 = ggplot() +
   layer_spatial(
     data = shape_parish,
     aes(fill = dist_east_west>0)
-  ) + 
+  ) +
   ggtitle("Distance East West")
-p1
+ggsave("tmp_plots/05_tmp4.png", p1)
 
-p1 = ggplot() + 
+p1 = ggplot() +
   layer_spatial(
     data = shape_parish,
     aes(fill = dist_east_west)
-  ) + 
+  ) +
   ggtitle("Distance East West")
-p1
+ggsave("tmp_plots/05_tmp5.png", p1)
 
 # Regions
 buffer = 20 # Middle zone size in km
@@ -169,13 +178,13 @@ shape_parish@data = shape_parish@data %>%
   ) %>%
   ungroup()
 
-p1 = ggplot() + 
+p1 = ggplot() +
   layer_spatial(
     data = shape_parish,
     aes(fill = limfjord_placement)
-  ) + 
+  ) +
   ggtitle("limfjord_placement")
-p1
+ggsave("tmp_plots/05_tmp6.png", p1)
 
 geo_data = shape_parish@data
 
@@ -200,3 +209,4 @@ geo_data = geo_data %>%
 
 # ==== Save result ====
 write_csv2(geo_data, "Data/Geo.csv")
+sf_use_s2(TRUE) # Re-enable after all spatial work is done
